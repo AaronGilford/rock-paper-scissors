7e5d7dba3600492bc9a3bfdd869019ab2d64e13a
AaronGilford <127493995+AaronGilford@users.noreply.github.com>
2023-04-17 12:08:54 +0100

    Add conditionals to compare computer and player choice and decide victor

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ function getComputerChoice() {
 let playerSelection = prompt("Rock, Paper or Scissors?")
 playerSelection = playerSelection.toLowerCase();
 
+let computerSelection = getComputerChoice();
+
+if (computerSelection == playerSelection) {
+    console.log("Draw! You chose the same as the computer")
+}
+else if (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors") {
+    console.log(`Invalid choice! You wrote ${playerSelection}`)
+}
+else if (computerSelection == "rock" && playerSelection == "paper" || computerSelection == "paper" && playerSelection == "scissors" || computerSelection === "scissors" && playerSelection == "rock") {
+    console.log(`You win! You chose ${playerSelection} and the computer chose ${computerSelection}`)
+}
+else {
+    console.log(`You lose! You chose ${playerSelection} and the computer chose ${computerSelection}`)
+}
+
