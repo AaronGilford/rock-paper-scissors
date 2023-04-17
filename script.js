a2d411287daf9da8d4e301e20df5a8de651c0f82
AaronGilford <127493995+AaronGilford@users.noreply.github.com>
2023-04-17 11:34:53 +0100

    Allow player to choose rock, paper or scissors.

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3) + 1;
     if (randomNumber === 1) {
-        return "Rock";
+        return "rock";
     } else if (randomNumber === 2) {
-        return "Paper";
+        return "paper";
     } else if (randomNumber === 3) {
         return "scissors"
     }
 }
+
+let playerSelection = prompt("Rock, Paper or Scissors?")
+playerSelection = playerSelection.toLowerCase();
+
