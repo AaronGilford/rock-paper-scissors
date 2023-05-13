580a328fcaa81a5399681ad629b795fbd6a967ce
AaronGilford <127493995+AaronGilford@users.noreply.github.com>
2023-05-13 10:48:38 +0100

    replace console logs

--- script.js
+++ script.js
@@ -9,11 +9,13 @@ function getComputerChoice() {
     }
 }
 
-let win = 0
-let draw = 0
-let lose = 0
+
 let computerSelection
 let playerSelection
+let rock = document.querySelector('#rock').addEventListener('click', game);
+let paper = document.querySelector('#paper').addEventListener('click', game);
+let scissors = document.querySelector('#scissors').addEventListener('click', game);
+let gameResult = document.querySelector('#results');
 
 function game(e) {
 
@@ -22,7 +24,7 @@ computerSelection = getComputerChoice();
 
 if (computerSelection == playerSelection) {
 
-    console.log("Draw! You chose the same as the computer")
+    gameResult.textContent = "Draw! You chose the same as the computer";
     draw++
 
 } else if (
@@ -31,7 +33,7 @@ if (computerSelection == playerSelection) {
     playerSelection != "scissors"
     ){
     
-    console.log(`Invalid choice! You wrote ${playerSelection}`)
+    gameResult.textContent = `Invalid choice! You wrote ${playerSelection}`;
 
 } else if (
     computerSelection == "rock" && 
@@ -39,19 +41,20 @@ if (computerSelection == playerSelection) {
     computerSelection == "paper" && 
     playerSelection == "scissors" || 
     computerSelection == "scissors" &&
-     playerSelection == "rock"
+    playerSelection == "rock"
      ){
 
-    console.log(`You win! You chose ${playerSelection} and the computer chose ${computerSelection}`)
+    gameResult.textContent = `You win! You chose ${playerSelection} and the computer chose ${computerSelection}`
     win++
 
 } else {
-    console.log(`You lose! You chose ${playerSelection} and the computer chose ${computerSelection}`)
+    gameResult.textContent = `You lose! You chose ${playerSelection} and the computer chose ${computerSelection}`;
     lose++
 }
 }
 
-let rock = document.querySelector('#rock').addEventListener('click', game);
-let paper = document.querySelector('#paper').addEventListener('click', game);
-let scissors = document.querySelector('#scissors').addEventListener('click', game);
-let gameResult = document.querySelector('#results');
+let win = 0
+let draw = 0
+let lose = 0
+
+
